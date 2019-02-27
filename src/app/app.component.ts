321199ce66ff4be811930a5e5f548d03feadf153
Rashmith Tula <lenovo@VAASUDEVA.sap.com>
2019-02-27 11:01:45 +0530

    My initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'formsReactive';
+  reactiveForm: FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      "projName" : new FormControl(null, Validators.required),
+      "email" : new FormControl(null, [Validators.required, Validators.email]),
+      "projStat" : new FormControl(null, Validators.required),
+      "persons" : new FormGroup({
+        "members" : new FormArray([new FormControl(null, Validators.required)])
+      }),
+    });
+  }
+
+  onSubmit() {
+   console.log(this.reactiveForm);
+  }
+
+  onAdd() {
+    (<FormArray>this.reactiveForm.get("persons.members")).push(new FormControl(null, Validators.required));
+  }
 }
